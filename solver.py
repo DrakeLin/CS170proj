4811f2b605b0d14cfb0b1b13b1fc399207ad75e5
chris-pan <chris.pan719@gmail.com>
2020-11-24 17:54:44 -0800

    phase 1

--- solver.py
+++ solver.py
@@ -181,11 +181,11 @@ if __name__ == '__main__':
     assert len(sys.argv) == 2
     path = sys.argv[1]
     G, s = read_input_file(path)
-    D, k = complete_solve(G, s)
-    #D, k = solve(G, s)
+    #D, k = complete_solve(G, s)
+    D, k = solve(G, s)
     assert is_valid_solution(D, G, s, k)
     print("Total Happiness: {}".format(calculate_happiness(D, G)))
-    write_output_file(D, 'out/test_complete.out')
+    write_output_file(D, 'out/50.out')
 
 
 # For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
